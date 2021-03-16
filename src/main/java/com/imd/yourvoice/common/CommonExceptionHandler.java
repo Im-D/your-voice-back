e62f47b3412bfd7b5990f1f7d2bd92d39a8aed7a
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-16 21:31:47 +0900

    refactor: HttpStatus 사용하도록 수정
    Spring에 기본 정의 돼있는 enum객체 사용 https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html 참조

--- src/main/java/com/imd/yourvoice/common/CommonExceptionHandler.java
+++ src/main/java/com/imd/yourvoice/common/CommonExceptionHandler.java
@@ -13,20 +13,20 @@ public class CommonExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-                .message("Validation Error")
     public ResponseDTO<ExceptionResponse> methodArgumentNotValidExceptionHandler(ContentCachingRequestWrapper request) {
         return ResponseDTO.<ExceptionResponse>builder()
                 .data(ExceptionResponse.of(request))
+                .message(HttpStatus.BAD_REQUEST.getReasonPhrase())
                 .isSuccess("fail")
                 .build();
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-                .message("UnknownRequestProperties Error")
     public ResponseDTO<ExceptionResponse> httpMessageNotReadableExceptionHandler(ContentCachingRequestWrapper request) {
         return ResponseDTO.<ExceptionResponse>builder()
                 .data(ExceptionResponse.of(request))
+                .message(HttpStatus.UNPROCESSABLE_ENTITY.getReasonPhrase())
                 .isSuccess("fail")
                 .build();
     }
--- src/main/java/com/imd/yourvoice/question/QuestionController.java
+++ src/main/java/com/imd/yourvoice/question/QuestionController.java
@@ -2,20 +2,24 @@ package com.imd.yourvoice.question;
 
 import com.imd.yourvoice.common.ResponseDTO;
 import com.imd.yourvoice.question.model.QuestionDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
 public class QuestionController {
+
     @PostMapping("/question")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO<QuestionDTO> createQuestion(@RequestBody @Valid QuestionDTO.CreateRequest questionDTO) {
         return ResponseDTO.<QuestionDTO>builder()
                 .data(questionDTO.toEntity().toDTO())
                 .isSuccess("success")
-                .message("CREATE SUCCESSFUL")
+                .message(HttpStatus.CREATED.getReasonPhrase())
                 .build();
     }
 }
